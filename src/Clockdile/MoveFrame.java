7dead705d550084f9a7028c12dd2d5311bcd1d3c
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2024-08-17 22:18:27 -0400

    Added a notice in MoveFrame

--- src/Clockdile/MoveFrame.java
+++ src/Clockdile/MoveFrame.java
@@ -6,13 +6,13 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 
+// ATTENTION: This class wasn't written by me (Kolyn090).
+// Unfortunately I'm currently unable to locate its source.
+// If you happen to know who wrote this class, please let
+// me know by forwarding a message to kolynlin@protonmail.com
 public class MoveFrame extends JFrame {
-
     public MoveFrame() {
-
-
         this.setUndecorated(true);
-
 
         FrameDragListener frameDragListener = new FrameDragListener(this);
         this.addMouseListener(frameDragListener);
@@ -21,7 +21,6 @@ public class MoveFrame extends JFrame {
         this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-
     }
 
     public static void main(String[] args) throws IOException {
@@ -29,7 +28,6 @@ public class MoveFrame extends JFrame {
     }
 
     public static class FrameDragListener extends MouseAdapter {
-
         private final JFrame frame;
         private Point mouseDownCompCoords = null;
 
